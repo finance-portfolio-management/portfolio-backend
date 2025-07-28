a839480d21354c6da499fb016c694cfd912b8fc0
ljmluotian <2571966792@qq.com>
2025-07-28 06:05:45 +0000

    this commit for put name

--- src/services/marketService.js
+++ src/services/marketService.js
@@ -2,19 +2,33 @@ import yahooFinance from "yahoo-finance2";
 
 
 const PRRSET_STOCKS = [
-    'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'META', 'TSLA',
-    'NVDA', 'JPM', 'NFLX', 'DIS', '600519.SS', '000858.SZ',
-    '601318.SS', '000333.SZ', '00700.HK', '00001.HK'
+    {id:1,symbol: 'AAPL', name:'Apple Inc.'},
+    {id:2,symbol: 'MSFT', name:'Microsoft Corporation'},
+    {id:3,symbol: 'GOOGL', name:'Alphabet Inc.'},
+    {id:4,symbol: 'AMZN', name:'Amazon.com Inc.'},
+    {id:5,symbol: 'TSLA', name:'Tesla Inc.'},
+    {id:6,symbol: 'NVDA', name:'NVIDIA Corporation'},
+    {id:7,symbol: '600519.SS', name:'guizhou Moutai'},
+    {id:8,symbol: '00700.HK', name:'Tecent Holdings'},
+    {id:9,symbol: 'NESTLE', name:'Nestle SA'},
+    {id:10,symbol: 'SHEL', name:'Shell plc'},
+    {id:11,symbol: 'NOVN', name:'Novartis AG'},
+    {id:12,symbol: 'SAP', name:'SAP SE'},
+    {id:13,symbol: 'UL', name:'Unilever PLC'},
+   
 ];
 
-const  getStockDailyReturn = async (symbol) => {
+const  getStockDailyReturn = async (stock) => {
     try {
+        const {symbol, name} = stock;
         const quote = await yahooFinance.quote(symbol);
         if (!quote?.regularMarketPrice || !quote?.regularMarketOpen)
             return null;
         const growthRate = ((quote.regularMarketPrice-quote.regularMarketOpen)/ quote.regularMarketOpen) * 100;
         return {
+            id: stock.id,
             symbol,
+            name,
             currentPrice: quote.regularMarketPrice.toFixed(2),
             growthRate: growthRate.toFixed(2)
         };
