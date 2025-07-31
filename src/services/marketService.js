bea3fc88ee5382a4b320dc90fffb0599053ebf47
ljmluotian <2571966792@qq.com>
2025-07-31 03:47:57 +0000

    this commit for updatePreset

--- src/services/marketService.js
+++ src/services/marketService.js
@@ -1,22 +1,39 @@
 import yahooFinance from "yahoo-finance2";
 
 
+
 const PRRSET_STOCKS = [
-    {id:1,symbol: 'AAPL', name:'Apple Inc.'},
-    {id:2,symbol: 'MSFT', name:'Microsoft Corporation'},
-    {id:3,symbol: 'GOOGL', name:'Alphabet Inc.'},
-    {id:4,symbol: 'AMZN', name:'Amazon.com Inc.'},
-    {id:5,symbol: 'TSLA', name:'Tesla Inc.'},
-    {id:6,symbol: 'NVDA', name:'NVIDIA Corporation'},
-    {id:7,symbol: '600519.SS', name:'guizhou Moutai'},
-    {id:8,symbol: '00700.HK', name:'Tecent Holdings'},
-    {id:9,symbol: 'NESTLE', name:'Nestle SA'},
-    {id:10,symbol: 'SHEL', name:'Shell plc'},
-    {id:11,symbol: 'NOVN', name:'Novartis AG'},
-    {id:12,symbol: 'SAP', name:'SAP SE'},
-    {id:13,symbol: 'UL', name:'Unilever PLC'},
-   
-];
+    { id: 1, symbol: 'AAPL', name: 'Apple Inc.' },
+    { id: 2, symbol: 'MSFT', name: 'Microsoft Corporation' },
+    { id: 3, symbol: 'GOOGL', name: 'Alphabet Inc.' },
+    { id: 4, symbol: 'AMZN', name: 'Amazon.com Inc.' },
+    { id: 5, symbol: 'TSLA', name: 'Tesla Inc.' },
+    { id: 6, symbol: 'NVDA', name: 'NVIDIA Corporation' },
+    { id: 7, symbol: 'META', name: 'Meta Platforms Inc.' },
+    { id: 8, symbol: 'NFLX', name: 'Netflix Inc.' },
+    { id: 9, symbol: 'AMD', name: 'Advanced Micro Devices' },
+    { id: 10, symbol: 'IBM', name: 'International Business Machines Corp.' },
+    { id: 11, symbol: 'COST', name: 'Costco Wholesale Corp.' },
+    { id: 12, symbol: 'WMT', name: 'Walmart Inc.' },
+    { id: 13, symbol: 'JPM', name: 'JPMorgan Chase & Co.' },
+    { id: 14, symbol: 'BAC', name: 'Bank of America Corp.' },
+    { id: 15, symbol: 'XOM', name: 'Exxon Mobil Corp.' },
+    { id: 16, symbol: 'CVX', name: 'Chevron Corp.' },
+    { id: 17, symbol: 'UNH', name: 'UnitedHealth Group Inc.' },
+    { id: 18, symbol: 'LLY', name: 'Eli Lilly and Co.' },
+    { id: 19, symbol: 'ABT', name: 'Abbott Laboratories' },
+    { id: 20, symbol: 'HD', name: 'Home Depot Inc.' },
+    { id: 21, symbol: 'INTC', name: 'Intel Corporation' },
+    { id: 22, symbol: 'DIS', name: 'The Walt Disney Company' },
+    { id: 23, symbol: 'T', name: 'AT&T Inc.' },
+    { id: 24, symbol: 'PFE', name: 'Pfizer Inc.'},
+    { id: 25, symbol: 'GE', name: 'General Electric Company' },
+    { id: 26, symbol: 'F', name: 'Ford Motor Company'},
+    { id: 27, symbol: 'GM', name: 'General Motors Company' },
+    { id: 28, symbol: 'MCD', name: 'McDonalds Corporation' },
+    { id: 29, symbol: 'KO', name: 'The Coca-Cola Company' },
+    { id: 30, symbol: 'HPQ', name: 'HP Inc.' }
+  ];
 
 const  getStockDailyReturn = async (stock) => {
     try {
